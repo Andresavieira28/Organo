5caac7adb31ad51d02245dfedc32ecf15298bd4a
andresavieira28 <andresacg_14@hotmail.com>
2023-03-21 14:55:55 -0300

    Incluindo props nos componentes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import Banner from './componentes/Banner/banner';
 import Form from './componentes/form/form'
-
 function App() {
   return (
     <div className="App">
--- src/componentes/form/form.js
+++ src/componentes/form/form.js
@@ -1,14 +1,33 @@
+import ListaSuspensa from "../ListaSuspensa/ListaSuspensa";
 import TextField from "../TextField/TextField";
+import Botao from "../botao/botao"
 import "./form.css";
 
 const Formulario = () => {
+  const times = [
+    'Programação',
+    'Front-End',
+    'Data Science',
+    'Devops',
+    'UX e Design',
+    'Mobile',
+    'Inovação e Gestão'
+  ]
+
+  const save = (e) => {
+    e.preventDefault()
+    console.log('o form foi submetido!')
+  }
+
   return (
     <section className="formulario">
-      <form>
+      <form onSubmit={save}>
         <h2>Preencha os dados para criar o card do colaborador</h2>
-        <TextField label="Nome" placeholder="Digite o seu nome" />
-        <TextField label="Cargo" placeholder="Digite o seu cargo" />
+        <TextField obrigatorio={true} label="Nome" placeholder="Digite o seu nome" />
+        <TextField obrigatorio={true} label="Cargo" placeholder="Digite o seu cargo" />
         <TextField label="Imagem" placeholder="Digite o endereço da sua imagem"/>
+        <ListaSuspensa obrigatorio={true} label="Time" itens={times}/>
+        <Botao children="Criar Card"/>
       </form>
     </section>
   );
