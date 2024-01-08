53cc5108244e81b15519dd95b03400677c58ed40
andresavieira28 <andresacg_14@hotmail.com>
2024-01-08 08:44:46 -0300

    componente dos times

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Banner from './componentes/Banner/banner';
 import Form from './componentes/form/form'
+import Time from './componentes/Time/time';
 
 function App() {
 
@@ -11,10 +12,55 @@ function App() {
     setColaboradores ([...colaboradores,colaborador])
   }
 
+  const times = [
+    {
+      nome: 'Programação',
+      corPrimaria: '#57C278',
+      corSecundaria: '#D9F7E9',
+    },
+    {
+      nome: 'Front-End',
+      corPrimaria: '#82CFFA',
+      corSecundaria: '#E8F8FF',
+    },
+    {
+      nome: 'Back-End',
+      corPrimaria: '#ffc26c',
+      corSecundaria: '#fff0da',
+    },
+    {
+      nome: 'Data Sciense',
+      corPrimaria: '#A6D157',
+      corSecundaria: '#F0F8E2',
+    },
+    {
+      nome: 'Devops',
+      corPrimaria: '#E06B69',
+      corSecundaria: '#FDE7E8',
+    },
+    {
+      nome: 'UX e Design',
+      corPrimaria: '#D86EBF',
+      corSecundaria: '#FAE5F5',
+    },
+   {
+      nome: 'Mobile',
+      corPrimaria: '#FEBA05',
+      corSecundaria: '#FFF5D9',
+   },
+   {
+      nome: 'Inovação e Gestão',
+      corPrimaria: '#FF8A29',
+      corSecundaria: '#FFEEDF',
+   }
+  
+  ]
+
   return (
     <div className="App">
       <Banner />
-      <Form aoColaboradorCadastrado = {colaboorador => aoNovoColaboradorAdd(colaboorador)}  />
+      <Form times={times.map(time => time.nome)} aoColaboradorCadastrado = {colaboorador => aoNovoColaboradorAdd(colaboorador)}/>
+      {times.map(time => <Time key={time.nome} nome= {time.nome} corPrimaria={time.corPrimaria} corSecundaria={time.corSecundaria} />)}
     </div>
   );
 }
