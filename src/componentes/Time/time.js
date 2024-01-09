1f0b3c07b763d483be2e97b4d693b2173c9ea4ee
andresavieira28 <andresacg_14@hotmail.com>
2024-01-08 22:06:03 -0300

    criando diretorio

--- src/componentes/Time/time.js
+++ src/componentes/Time/time.js
@@ -1,21 +1,23 @@
-import Colaborador from '../Colaborador/colaborador'
-import './time.css'
+import Colaborador from "../Colaborador/colaborador";
+import "./time.css";
 
 const Time = (props) => {
+  const css = { backgroundColor: props.corSecundaria };
 
-    const css = {backgroundColor:props.corSecundaria}
+  return (
+    <section className="time" style={css}>
+      <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
+      <div className="colaboradores">
+        {props.colaboradores.map((colaborador) => (
+          <Colaborador
+            nome={colaborador.nome}
+            cargo={colaborador.cargo}
+            imagem={colaborador.imagem}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
 
-    return(
-        <section className='time' style={css}>
-            <h3 style={{borderColor: props.corPrimaria}}>{props.nome}</h3>
-            
-            {props.colaboradores.map(colaborador => <Colaborador 
-                nome={colaborador.nome} 
-                cargo={colaborador.cargo} 
-                imagem={colaborador.imagem}
-            />)}
-        </section>
-    )
-}
-
-export default Time
+export default Time;
