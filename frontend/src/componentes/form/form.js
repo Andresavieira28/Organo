b5093e37a9db8c439dee87189ccd345fff7b5b61
Andresa Vieira <andresacg_14@hotmail.com>
2025-05-25 22:43:31 -0300

    refresh form

--- frontend/src/componentes/form/form.js
+++ frontend/src/componentes/form/form.js
@@ -1,63 +1,80 @@
 import ListaSuspensa from "../ListaSuspensa/ListaSuspensa";
 import TextField from "../TextField/TextField";
-import Botao from "../botao/botao"
+import Botao from "../botao/botao";
 import "./form.css";
 import { useState } from "react";
 
 const Formulario = (props) => {
-
-  const [nome, setNome] = useState('')
-  const [cargo, setCargo] = useState('')
-  const [imagem, setImagem] = useState('')
-  const [time, setTime] = useState('')
+  const [nome, setNome] = useState("");
+  const [cargo, setCargo] = useState("");
+  const [imagem, setImagem] = useState("");
+  const [time, setTime] = useState("");
 
   const save = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     props.aoColaboradorCadastrado({
       nome,
       cargo,
       imagem,
-      time
-    })
-    setNome('')
-    setCargo('')
-    setImagem('')
-    setTime('')
-  }
+      time,
+    });
+    setNome("");
+    setCargo("");
+    setImagem("");
+    setTime("");
+  };
 
   return (
-    <section className="formulario">
-      <form onSubmit={save}>
-        <h2>Preencha os dados para criar o card do colaborador</h2>
-        <TextField 
-          obrigatorio={true}
-          label="Nome" 
-          placeholder="Digite o seu nome" 
-          valor={nome} 
-          aoAlterado={valor => setNome(valor)} />
+    <section className="formulario-container">
+      <form className="formulario" onSubmit={save}>
+        <h2 className="formulario__titulo">Preencha os dados do colaborador</h2>
 
-        <TextField 
-          obrigatorio={true} 
-          label="Cargo" 
-          placeholder="Digite o seu cargo"
-          valor={cargo} 
-          aoAlterado={valor => setCargo(valor)}  />
-
-        <TextField 
-          label="Imagem" 
-          placeholder="Digite o endereço do seu GitHub + .png"
-          valor={imagem} 
-          aoAlterado={valor => setImagem(valor)} />
-
-        <ListaSuspensa 
-          obrigatorio={true} 
-          label="Time"
-          itens={props.times}
-          valor = {time}
-          aoAlterado= {valor => setTime(valor)}
+        <div className="formulario__campo">
+          <label htmlFor="nome-colaborador">Nome</label>
+          <TextField
+            id="nome-colaborador"
+            obrigatorio={true}
+            placeholder="Digite o seu nome"
+            valor={nome}
+            aoAlterado={(valor) => setNome(valor)}
           />
+        </div>
 
-        <Botao children="Criar Card"/>
+        <div className="formulario__campo">
+          <label htmlFor="cargo-colaborador">Cargo</label>
+          <TextField
+            id="cargo-colaborador"
+            obrigatorio={true}
+            placeholder="Digite o seu cargo"
+            valor={cargo}
+            aoAlterado={(valor) => setCargo(valor)}
+          />
+        </div>
+
+        <div className="formulario__campo">
+          <label htmlFor="imagem-colaborador">Imagem</label>
+          <TextField
+            id="imagem-colaborador"
+            placeholder="Endereço do seu GitHub + .png"
+            valor={imagem}
+            aoAlterado={(valor) => setImagem(valor)}
+          />
+        </div>
+
+        <div className="formulario__campo">
+          <label htmlFor="time-colaborador">Time</label>
+          <ListaSuspensa
+            id="time-colaborador"
+            obrigatorio={true}
+            itens={props.times}
+            valor={time}
+            aoAlterado={(valor) => setTime(valor)}
+          />
+        </div>
+
+        <div className="formulario__acao">
+          <Botao>Criar Card</Botao>
+        </div>
       </form>
     </section>
   );
